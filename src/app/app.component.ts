b194817e30735676cfa429bea497c3220de77dab
albertoparente <albertoparentefh@gmail.com>
2021-10-23 22:26:25 -0300

    Deleting unnecessary information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Photo Manager';
+  description = 'Photo';
+  url = 'https://ci-prod.imgix.net/upload/paises/00200408.jpg?w=2520&fit=crop&auto=compress%2Cformat'
 }
