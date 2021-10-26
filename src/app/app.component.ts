f2fe87f2a9d154bf4025cc7c592798f5bf986329
albertoparente <albertoparentefh@gmail.com>
2021-10-25 23:28:40 -0300

    Creating photo component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Photo Manager';
-  description = 'Photo';
-  url = 'https://ci-prod.imgix.net/upload/paises/00200408.jpg?w=2520&fit=crop&auto=compress%2Cformat'
 }
