86433d2b4c02397c94fd36f556bee08e8a2447bb
albertoparente <albertoparentefh@gmail.com>
2021-10-26 23:17:11 -0300

    Using ngfor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    photos = [
+        {
+            url: "https://ci-prod.imgix.net/upload/paises/00200408.jpg?w=2520&fit=crop&auto=compress%2Cformat",
+            description: "Img1"
+        },
+        {
+            url: "https://www.viajoteca.com/wp-content/uploads/2018/06/roteiro-canada-pexel-andre-scaled.jpg",
+            description: "Img2"
+        },
+        {
+            url: "https://ci-prod.imgix.net/upload/paises/00200408.jpg?w=2520&fit=crop&auto=compress%2Cformat",
+            description: "Img1"
+        },
+        {
+            url: "https://www.viajoteca.com/wp-content/uploads/2018/06/roteiro-canada-pexel-andre-scaled.jpg",
+            description: "Img2"
+        }
+    ];
 }
