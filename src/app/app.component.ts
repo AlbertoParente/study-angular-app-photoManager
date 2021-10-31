8393a15032bbc7c671cef64fd1296d23cbc7f8e5
albertoparente <albertoparentefh@gmail.com>
2021-10-30 23:35:56 -0300

    Adjusting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
@@ -6,12 +6,13 @@ import { PhotoService } from './photos/photo/photo.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     photos: Object[] = [];
 
-    constructor(photoService: PhotoService) {
+    constructor(private photoService: PhotoService) { }
 
-        photoService
+    ngOnInit() {
+        this.photoService
             .listFromUser('flavio')
             .subscribe(photos => this.photos = photos);
     }
