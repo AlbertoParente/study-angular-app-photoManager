2c071fcda506364cf086ed2dc1cbf2cb1b4e4a2e
albertoparente <albertoparentefh@gmail.com>
2021-11-03 23:29:12 -0300

    Adjusting User Route and Adjusting Photo List Layout

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -10,11 +11,15 @@ export class PhotoListComponent implements OnInit {
 
     photos: Object[] = [];
 
-    constructor(private photoService: PhotoService) { }
+    constructor(
+        private photoService: PhotoService,
+        private activatedRoute: ActivatedRoute
+    ) { }
 
     ngOnInit() {
+        const userName = this.activatedRoute.snapshot.params.userName;
         this.photoService
-            .listFromUser('flavio')
+            .listFromUser(userName)
             .subscribe(photos => this.photos = photos);
     }
 }
