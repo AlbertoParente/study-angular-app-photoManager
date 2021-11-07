589353a1f573fb4a8312315ac6c3318b987b26b0
albertoparente <albertoparentefh@gmail.com>
2021-11-07 20:07:07 -0300

    Implementing grid logic

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -8,10 +8,21 @@ import { Photo } from '../../photo/photo';
 })
 export class PhotosComponent implements OnInit {
     @Input() photos: Photo[] = [];
+    rows = [];
 
     constructor() { }
 
     ngOnInit() {
+        this.rows = this.groupColumns(this.photos)
     }
 
+    groupColumns(photos: Photo[]) {
+        const newRows = [];
+
+        for(let i = 0; i < photos.length; i += 3 ) {
+            newRows.push(photos.slice(i, i +3));
+        }
+
+        return newRows;
+    }
 }
