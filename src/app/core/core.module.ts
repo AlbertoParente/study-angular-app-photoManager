bd7ea795d36e50129b063ba025a22a5f2fe6a2f1
albertoparente <albertoparentefh@gmail.com>
2021-12-03 22:21:14 -0300

    Creating header aplication

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core'
+import { headerComponent } from './header/header.component';
+
+@NgModule({
+    declarations: [headerComponent],
+    exports: [headerComponent]
+})
+export class CoreModule {}
