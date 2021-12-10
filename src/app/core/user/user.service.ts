6abc69a2e2a7d9dc10d46914bc84b8dbdecf0513
albertoparente <albertoparentefh@gmail.com>
2021-12-09 21:22:30 -0300

    Adding routerLink

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -26,7 +26,7 @@ export class UserService {
 
     private decodeAndNotify() {
         const token = this.tokenService.getToken();
-        const user = jtw_decode(token) as User;
+        const user = jwt_decode(token) as User;
         this.userName = user.name;
         this.userSubject.next(user);
     }
