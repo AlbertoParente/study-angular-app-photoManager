15358c294ea6ae5f5ce6eef3bb05078455b7acff
albertoparente <albertoparentefh@gmail.com>
2022-02-25 21:43:48 -0300

    looking for comments

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Observable } from 'rxjs';
 import { Photo } from '../photo/photo';
+import { PhotoComment } from '../photo/photo-comment';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
     templateUrl: './photo-detail.component.html',
+    styleUrls:['photo-details.css']
 })
 export class PhotoDetailsComponent implements OnInit {
 
-    photo: Photo;
+    photo$: Observable<Photo>
+    comments$: Observable<PhotoComment[]>
 
     constructor(
         private route: ActivatedRoute,
@@ -16,9 +20,8 @@ export class PhotoDetailsComponent implements OnInit {
     ) { }
 
     ngOnInit(): void {
-        const id = this.route.snapshot.params.photoId;
-        this.photoService
-            .findById(id)
-            .subscribe(photo => this.photo = photo);
+        const photoId = this.route.snapshot.params.photoId
+        this.photo$ = this.photoService.findById(photoId)
+        this.comments$ = this.photoService.getComments(photoId)
     }
 }
