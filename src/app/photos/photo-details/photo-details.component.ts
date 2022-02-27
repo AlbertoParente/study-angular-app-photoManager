d15778519d5da4b1a85ae265317d2a0a6067f936
albertoparente <albertoparentefh@gmail.com>
2022-02-26 23:33:01 -0300

    Componentizing comments

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -2,17 +2,16 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Observable } from 'rxjs';
 import { Photo } from '../photo/photo';
-import { PhotoComment } from '../photo/photo-comment';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
     templateUrl: './photo-detail.component.html',
-    styleUrls:['photo-details.css']
+    styleUrls: ['photo-details.css']
 })
 export class PhotoDetailsComponent implements OnInit {
 
     photo$: Observable<Photo>
-    comments$: Observable<PhotoComment[]>
+    photoId: number
 
     constructor(
         private route: ActivatedRoute,
@@ -20,8 +19,7 @@ export class PhotoDetailsComponent implements OnInit {
     ) { }
 
     ngOnInit(): void {
-        const photoId = this.route.snapshot.params.photoId
-        this.photo$ = this.photoService.findById(photoId)
-        this.comments$ = this.photoService.getComments(photoId)
+        this.photoId = this.route.snapshot.params.photoId
+        this.photo$ = this.photoService.findById(this.photoId)
     }
 }
