980e85f947a029b554c7c1c33c91247669289372
albertoparente <albertoparentefh@gmail.com>
2022-03-28 23:33:05 -0300

    Adjusting

--- src/app/photos/photo-list/load-button/load-button.component.spec.ts
+++ src/app/photos/photo-list/load-button/load-button.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoadButtonComponent } from './load-button.component';
 
 describe('LoadButtonComponent', () => {
--- src/app/photos/photo-list/photo-list.component.spec.ts
+++ src/app/photos/photo-list/photo-list.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PhotoListComponent } from './photo-list.component';
 
 describe('PhotoListComponent', () => {
